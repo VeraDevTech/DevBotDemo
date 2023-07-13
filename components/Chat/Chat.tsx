93ca383ff05ff7158c7e918efd1921ceefc9b56b
VeraDevTech <vera.ebel@devtechnology.com>
2023-07-13 07:02:50 -0400

    Cleaning up typos and changing default to light

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -352,17 +352,17 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       {!(apiKey || serverSideApiKeyIsSet) ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
-            Welcome to DevBot UI
+            DevBot
           </div>
           <div className="text-center text-lg text-black dark:text-white">
             <div className="mb-8">{`DevBot UI is an open source clone of OpenAI's ChatGPT UI.`}</div>
             <div className="mb-2 font-bold">
-              Important: DevBot UI is 100% unaffiliated with OpenAI.
+              Important: DevBot is 100% unaffiliated with OpenAI.
             </div>
           </div>
           <div className="text-center text-gray-500 dark:text-gray-400">
             <div className="mb-2">
-              DevBot UI allows you to plug in your API key to use this UI with
+              Chatbot UI allows you to plug in your API key to use this UI with
               their API.
             </div>
             <div className="mb-2">
@@ -405,7 +405,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
                         <Spinner size="16px" className="mx-auto" />
                       </div>
                     ) : (
-                      'DevBot UI'
+                      'DevBot'
                     )}
                   </div>
 
